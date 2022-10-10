7d0b92a90852ce0c07bbd36bfba26e1ff42fcfbd
renangiaretta <ranan.giaretta@hotmail.com>
2022-10-10 05:10:58 -0300

    entrega

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,125 @@
-/* Desenvolva sua lógica aqui */
+const listValues = document.createElement('ul')
+listValues.id = 'ulFull'
+const filterAllButton = document.getElementById('filterAll')
+const filterEntryMoneyButton = document.getElementById('filterEntrada')
+const filterOutMoneyButton = document.getElementById('filterSaida')
+const valuesCategory = ["Entrada", "Saída"];
+
+let insertedValues = [];
+
+let renderList = (valuesList) => {
+  let main = document.getElementById('app')
+
+  if (valuesList.length == 0){
+      listValues.innerHTML = ''
+      listValues.classList = 'full_w'
+      let emptyListContainer = document.createElement('div')
+      let emptyListText1 = document.createElement('span')
+      let emptyListText2 = document.createElement('span')
+
+      emptyListText1.innerText = 'Nenhum valor registrado'
+      emptyListText2.innerText = 'Registrar novo valor'
+
+      emptyListContainer.classList = 'empty_list full_w flex__column justify__around align__center gap-6 backgroundColor-1'
+      emptyListText1.classList = 'text2-bold'
+      emptyListText2.classList = 'text2-medium'
+
+      emptyListContainer.append(emptyListText1, emptyListText2)
+      listValues.append(emptyListContainer)
+      main.append(listValues)
+  }
+  else{   
+          listValues.innerHTML = ''
+          listValues.classList = 'full_w flex__column gap-5'
+
+          valuesList.forEach(listItem => {
+              let listItems = document.createElement('li')
+              let listItemsValues = document.createElement('span')
+              let listItemsContainer = document.createElement('div')
+              let listItemsDivCategory = document.createElement('span')
+              let listItemsRemoveButton = document.createElement('button')
+              let listItemsRemoveButtonImg = document.createElement('img')
+
+              listItems.classList =  'list_section__li flex__row justify__between align__center backgroundColor-3 full_w'
+              listItemsValues.classList = 'text2-medium'
+              listItemsContainer.classList = 'list_section__li__div flex__row justify__between align__center gap-4'
+              listItemsDivCategory.classList = 'list_section__li__div-span backgroundColor-2 text2-regular'
+              listItemsRemoveButton.classList = 'button__trash'
+              listItemsRemoveButtonImg.classList = "button_trash_img"
+
+              listItemsValues.innerText = `R$ ${listItem.value}`
+              if(listItem.categoryID == 0){
+                listItemsDivCategory.innerText = valuesCategory[0]
+              }else if(listItem.categoryID == 1){
+                listItemsDivCategory.innerText = valuesCategory[1]
+              }
+              listItemsRemoveButtonImg.src = '/assets/trash.png'
+
+              listItemsRemoveButton.addEventListener('click', function(){
+                  listValues.innerHTML = ''
+                  let findIndex = valuesList.indexOf(listItem)
+                  if(valuesList == insertedValues){
+                    insertedValues.splice(findIndex, 1)  
+                    valuesSum(insertedValues)
+                    renderList(valuesList) 
+                  }else{
+                    insertedValues.splice(findIndex, 1)
+                    valuesList.splice(findIndex, 1)
+                    valuesSum(valuesList)
+                    renderList(valuesList) 
+                  }                                                      
+              })
+              
+              listItemsRemoveButton.append(listItemsRemoveButtonImg)
+              listItemsContainer.append(listItemsDivCategory, listItemsRemoveButton)
+              listItems.append(listItemsValues, listItemsContainer)
+              listValues.append(listItems)
+          });
+
+      return main.append(listValues)
+  }
+}
+
+renderList(insertedValues)
+
+let valuesSum = (listOfValues) => {
+    const showSum = document.getElementById('sectionSum')
+    const value = document.createElement('span')
+    const totalValue = document.createElement('span')
+
+          totalValue.classList = 'text2-medium'
+          value.classList = 'text2-medium'
+          totalValue.innerText = "Valor total:"
+      if(listOfValues.length == 0){
+        value.innerText = `R$ ${0},00`
+      }else{
+        let valuesMapping = listOfValues.map((listItem) => listItem.value)
+        let sum =  valuesMapping.reduce((previousValue, actualValue) => previousValue + actualValue, 0)
+        value.innerText = `R$${sum}`
+      }
+    showSum.innerHTML='' 
+    showSum.append(totalValue,value)
+}
+
+valuesSum(insertedValues)
+
+filterAllButton.addEventListener('click', function(){
+    listValues.innerHTML = ''
+    valuesSum(insertedValues)
+    renderList(insertedValues)
+})
+
+
+filterEntryMoneyButton.addEventListener('click', function(){
+  let filteringEntryMoney = insertedValues.filter(listItem => listItem.categoryID == 0)
+  listValues.innerHTML = ''
+  valuesSum(filteringEntryMoney)
+  renderList(filteringEntryMoney)
+})
+
+filterOutMoneyButton.addEventListener('click', function(){
+  let filteringOutMoney = insertedValues.filter(listItem => listItem.categoryID == 1)
+  listValues.innerHTML = ''
+  valuesSum(filteringOutMoney)
+  renderList(filteringOutMoney)
+})
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,129 @@
-/* Desenvolva sua lógica aqui */
+const valueRegisterButton = document.getElementById('btnRegistry')
+
+valueRegisterButton.addEventListener('click', function(){
+
+    const selectBody = document.querySelector('body')
+    const modalCasing = document.createElement('div')
+    const modalContainer = document.createElement('div')
+    const modalDivPlace = document.createElement('div')
+    const modalTitle = document.createElement('span')
+    const modalClose = document.createElement('button')
+    const modalDivera = document.createElement('div')
+    const modalText = document.createElement('p')
+    const modalInputs = document.createElement('div')
+    const modalInsertValueText = document.createElement('span')
+    const modalImputValue = document.createElement('input')
+    const modalValueTypeContainer = document.createElement('div')
+    const modalValueTypeText = document.createElement('span')
+    const modalValueTypeButtons = document.createElement('div')
+    const modalEntryMoneyButton = document.createElement('button')
+    const modalOutMoneyButton = document.createElement('button')
+    const modalFinalButtons = document.createElement('div')
+    const modalCancelButton = document.createElement('button')
+    const modal = document.createElement('button')
+  
+    modalImputValue.type = "number"
+    modalImputValue.required = true
+    modalImputValue.id = "inputAddValue"
+  
+    modalCasing.classList = 'modalWrapper flex__row justify__center align__center full_w full_h'
+    modalContainer.classList = 'modal flex__column'
+    modalDivPlace.classList = 'flex__row justify__between align__center'
+    modalTitle.classList = 'title2-bold' 
+    modalClose.classList = 'modal_close-button'
+    modalDivera.classList = 'text1-bold'
+    modalText.classList = ''
+    modalInputs.classList = 'modal_inputs flex__column'
+    modalInsertValueText.classList = 'text2-medium'
+    modalImputValue.classList = 'modal_inputs-input full_w'
+    modalValueTypeContainer.classList = 'flex__column desktop_modal'
+    modalValueTypeText.classList = 'text2-medium mb-2'
+    modalValueTypeButtons.classList = 'moda_value_type_container full_w flex__column desktop_modal'
+    modalEntryMoneyButton.classList = 'moda_value_type_container-button full_w text3-medium desktop_modal_divOne-button-1'
+    modalOutMoneyButton.classList = 'moda_value_type_container-button full_w text3-medium desktop_modal_divOne-button-2'
+    modalFinalButtons.classList = 'modal_final_buttons_container full_w flex__row justify__between desktop_modal_divTwo'
+    modalCancelButton.classList = 'modal_final_buttons_container__btn-1 text3-medium desktop_modal_buttons-2'
+    modal.classList = 'modal_final_buttons_container__btn-2 text3-medium desktop_modal_buttons-2'
+
+    modalCasing.id = 'wrapper'
+  
+    modalImputValue.placeholder = "R$"
+    modalTitle.innerText = 'Registro de valor'
+    modalClose.innerText = 'X'
+    modalText.innerText = 'Digite o valor e em seguida aperte no botão referente ao tipo do valor'
+    modalInsertValueText.innerText = 'Valor'
+    modalValueTypeText.innerText = 'Tipo de valor'
+    modalEntryMoneyButton.innerText = 'Entrada'
+    modalOutMoneyButton.innerText = 'Saída'
+    modalCancelButton.innerText = 'Cancelar'
+    modal.innerText = 'Inserir valor'
+  
+    modalEntryMoneyButton.addEventListener('click', function(){
+        if(modalOutMoneyButton.innerText == "Saída!"){
+            modalOutMoneyButton.innerText = "Saída"
+            modalEntryMoneyButton.innerText = 'Entrada!'
+        }else{
+            modalEntryMoneyButton.innerText = 'Entrada!'
+        }
+    })
+
+    modalOutMoneyButton.addEventListener('click', function(){
+        if(modalEntryMoneyButton.innerText == "Entrada!"){
+            modalEntryMoneyButton.innerText = "Entrada"
+            modalOutMoneyButton.innerText = 'Saída!'
+        }else{
+            modalOutMoneyButton.innerText = 'Saída!'
+        }
+    })
+
+    let counterIDs = 0
+    let registerNewValues = () => {
+  
+    let wrapper = document.getElementById('wrapper')
+    let counterUpdated = counterIDs++
+    listValues.innerHTML = ''
+    if(modalEntryMoneyButton.innerText == "Entrada!"){
+    let newObject = {
+        id:counterUpdated,
+        value: +modalImputValue.value,
+        categoryID: 0,
+    }
+        insertedValues.push(newObject)
+    }else if(modalOutMoneyButton.innerText = 'Saída!'){
+        let newObject = {
+            id:counterUpdated,
+            value: +modalImputValue.value,
+            categoryID: 1,
+        }
+            insertedValues.push(newObject)
+    }
+    
+    valuesSum(insertedValues)
+    renderList(insertedValues)
+    wrapper.remove()
+    }
+  
+    modal.addEventListener('click', registerNewValues)
+  
+  
+    modalClose.addEventListener('click', function(){
+        let wrapper = document.getElementById('wrapper')
+        return wrapper.remove()
+    })
+  
+    modalCancelButton.addEventListener('click', function(){
+        let wrapper = document.getElementById('wrapper')
+        return wrapper.remove()
+    })
+  
+    modalValueTypeButtons.append(modalEntryMoneyButton, modalOutMoneyButton)
+    modalFinalButtons.append(modalCancelButton, modal)
+    modalValueTypeContainer.append(modalValueTypeText, modalValueTypeButtons, modalFinalButtons)
+    modalInputs.append(modalInsertValueText, modalImputValue)
+    modalDivera.append(modalText)
+    modalDivPlace.append(modalTitle, modalClose)
+    modalContainer.append(modalDivPlace, modalDivera, modalInputs, modalValueTypeContainer)
+    modalCasing.append(modalContainer)
+  
+    selectBody.append(modalCasing)
+})
